ada81b5b2c54a86e14d74c24ed46c3e99a4aa3a4
hwimin-kim <gnlals7841@gmail.com>
2022-05-12 14:26:04 +0900

    first commit

---
+++ sql-practices/sql연습/DML-Tutorials.sql
@@ -0,0 +1,8 @@
+-- Basic Query
+
+select version(), current_date(), current_time();
+
+-- 대소문자 구분이 없다.
+SELECT VERSION(), CURRENT_DATE(), CURRENT_TIME();
+select vErSion(), current_DATE(), current_TIME();
+
