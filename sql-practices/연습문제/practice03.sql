43d2901027fcaf78f14ce0c3252cdf41fcd61dcc
hwimin-kim <gnlals7841@gmail.com>
2022-05-17 16:58:14 +0900

    practice03, 04 commit

--- sql-practices/연습문제/practice03.sql
+++ sql-practices/연습문제/practice03.sql
@@ -2,30 +2,144 @@
 
 -- 문제 1. 
 -- 현재 급여가 많은 직원부터 직원의 사번, 이름, 그리고 연봉을 출력 하시오.
-
+SELECT 
+    a.emp_no, a.first_name, b.salary
+FROM
+    employees a,
+    salaries b
+WHERE
+    a.emp_no = b.emp_no
+        AND b.to_date LIKE '9999%'
+ORDER BY b.salary DESC;
+        
 -- 문제2.
 -- 전체 사원의 사번, 이름, 현재 직책을 이름 순서로 출력하세요.
+SELECT 
+    a.emp_no, a.first_name, b.title
+FROM
+    employees a,
+    titles b
+WHERE
+    a.emp_no = b.emp_no
+        AND b.to_date LIKE '9999%'
+ORDER BY a.first_name ASC;
 
 -- 문제3.
 -- 전체 사원의 사번, 이름, 현재 부서를 이름 순서로 출력하세요..
+SELECT 
+    a.emp_no, a.first_name, c.dept_name
+FROM
+    employees a,
+    dept_emp b,
+    departments c
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND b.to_date LIKE '9999%'
+ORDER BY a.first_name ASC;
 
 -- 문제4.
 -- 전체 사원의 사번, 이름, 연봉, 직책, 부서를 모두 이름 순서로 출력합니다.
+SELECT 
+    a.emp_no, CONCAT(a.first_name, ' ', a.last_name) AS '직원명', b.salary, c.title, e.dept_name
+FROM
+    employees a,
+    salaries b,
+    titles c,
+    dept_emp d,
+    departments e
+WHERE
+    a.emp_no = b.emp_no
+        AND b.emp_no = c.emp_no
+        AND a.emp_no = d.emp_no
+        AND d.dept_no = e.dept_no
+ORDER BY a.first_name;
 
 -- 문제5.
 -- ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요. (현재 ‘Technique Leader’의 직책(으로 근무하는 사원은 고려하지 않습니다.) 이름은 first_name과 last_name을 합쳐 출력 합니다.
-
+SELECT 
+    a.emp_no, CONCAT(a.first_name, ' ', a.last_name) AS '직원명'
+FROM
+    employees a,
+    titles b
+WHERE
+    a.emp_no = b.emp_no
+        AND b.title = 'Technique Leader'
+        AND b.to_date != '9999-01-01';
+        
 -- 문제6.
 -- 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
-
+SELECT 
+    CONCAT(a.first_name, ' ', a.last_name) AS '직원명',
+    c.dept_name,
+    d.title
+FROM
+    employees a,
+    dept_emp b,
+    departments c,
+    titles d
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND a.emp_no = d.emp_no
+        AND a.last_name LIKE 'S%';
+        
 -- 문제7.
 -- 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.
-
+SELECT 
+    a.emp_no, a.title, b.salary
+FROM
+    titles a,
+    salaries b
+WHERE
+    a.emp_no = b.emp_no
+        AND a.to_date LIKE '9999%'
+        AND b.to_date LIKE '9999%'
+        AND a.title = 'Engineer'
+        AND b.salary >= 40000
+ORDER BY salary DESC;
+    
 -- 문제8.
 -- 현재 급여가 50000이 넘는 직책을 직책, 급여로 급여가 큰 순서대로 출력하시오
+SELECT 
+    b.title, a.salary
+FROM
+    salaries a,
+    titles b
+WHERE
+    a.emp_no = b.emp_no
+        AND a.to_date LIKE '9999%'
+        AND b.to_date LIKE '9999%'
+        AND a.salary > 50000
+ORDER BY a.salary DESC;
 
 -- 문제9.
 -- 현재, 부서별 평균 연봉을 연봉이 큰 부서 순서대로 출력하세요.
+SELECT 
+    b.dept_name AS '부서명', AVG(c.salary) AS '평균\n연봉'
+FROM
+    dept_emp a,
+    departments b,
+    salaries c
+WHERE
+    a.dept_no = b.dept_no
+        AND a.emp_no = c.emp_no
+        AND a.to_date LIKE '9999%'
+        AND c.to_date LIKE '9999%'
+GROUP BY b.dept_name
+ORDER BY AVG(c.salary) DESC;
 
 -- 문제10.
 -- 현재, 직책별 평균 연봉을 연봉이 큰 직책 순서대로 출력하세요.
+SELECT 
+    a.title AS '직책',
+    AVG(b.salary) AS '평균\n연봉'
+FROM
+    titles a,
+    salaries b
+WHERE
+    a.emp_no = b.emp_no
+        AND a.to_date LIKE '9999%'
+        AND b.to_date LIKE '9999%'
+GROUP BY a.title
+ORDER BY AVG(b.salary) DESC;
--- sql-practices/연습문제/practice04.sql
+++ sql-practices/연습문제/practice04.sql
@@ -2,25 +2,267 @@
 
 -- 문제1.
 -- 현재 평균 연봉보다 많은 월급을 받는 직원은 몇 명이나 있습니까?
+-- SELECT 
+--     AVG(salary) AS avg_salary
+-- FROM
+--     salaries
+-- WHERE
+--     to_date LIKE '9999%';
+SELECT 
+    COUNT(*)
+FROM
+    salaries
+WHERE
+    to_date LIKE '9999%'
+        AND salary > (SELECT 
+            AVG(salary) AS avg_salary
+        FROM
+            salaries
+        WHERE
+            to_date LIKE '9999%');
 
 -- 문제2. 
 -- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. 
+-- SELECT 
+--     a.dept_no, MAX(b.salary)
+-- FROM
+--     dept_emp a,
+--     salaries b
+-- WHERE
+--     a.emp_no = b.emp_no
+--         AND a.to_date LIKE '9999%'
+--         AND b.to_date LIKE '9999%'
+-- GROUP BY a.dept_no;
+SELECT 
+    a.emp_no, concat(a.first_name, ' ',a.last_name) as emp_fullname, c.dept_name, d.salary
+FROM
+    employees a,
+    dept_emp b,
+    departments c,
+    salaries d
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND a.emp_no = d.emp_no
+        AND b.to_date LIKE '9999%'
+        AND d.to_date LIKE '9999%'
+        AND (c.dept_no , d.salary) IN (SELECT 
+            a.dept_no, MAX(b.salary)
+        FROM
+            dept_emp a,
+            salaries b
+        WHERE
+            a.emp_no = b.emp_no
+                AND a.to_date LIKE '9999%'
+                AND b.to_date LIKE '9999%'
+        GROUP BY a.dept_no)
+ORDER BY d.salary;
 
 -- 문제3.
 -- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요 
-
+-- SELECT 
+--     a.dept_no, AVG(b.salary) AS avg_salary
+-- FROM
+--     dept_emp a,
+--     salaries b
+-- WHERE
+--     a.emp_no = b.emp_no
+--         AND a.to_date LIKE '9999%'
+--         AND b.to_date LIKE '9999%'
+-- GROUP BY a.dept_no;
+SELECT 
+    a.emp_no, concat(a.first_name, ' ',a.last_name) as emp_fullname, b.salary, d.avg_salary
+FROM
+    employees a,
+    salaries b,
+    dept_emp c,
+    (SELECT 
+        a.dept_no, AVG(b.salary) AS avg_salary
+    FROM
+        dept_emp a, salaries b
+    WHERE
+        a.emp_no = b.emp_no
+            AND a.to_date LIKE '9999%'
+            AND b.to_date LIKE '9999%'
+    GROUP BY a.dept_no) d
+WHERE
+    a.emp_no = b.emp_no
+        AND b.emp_no = c.emp_no
+        AND c.dept_no = d.dept_no
+        AND b.to_date LIKE '9999%'
+        AND c.to_date LIKE '9999%'
+        AND b.salary > d.avg_salary;
+        
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
+-- SELECT 
+--     b.dept_no,
+--     CONCAT(a.first_name, ' ', a.last_name) AS manager_name
+-- FROM
+--     employees a,
+--     dept_manager b
+-- WHERE
+--     a.emp_no = b.emp_no
+--         AND b.to_date LIKE '9999%';
+SELECT 
+    a.emp_no, concat(a.first_name, ' ',a.last_name) as emp_fullname, m.manager_name, b.dept_name
+FROM
+    employees a,
+    departments b,
+    dept_emp c,
+    (SELECT 
+        b.dept_no,
+            CONCAT(a.first_name, ' ', a.last_name) AS manager_name
+    FROM
+        employees a, dept_manager b
+    WHERE
+        a.emp_no = b.emp_no
+            AND b.to_date LIKE '9999%') m
+WHERE
+    a.emp_no = c.emp_no
+        AND c.dept_no = b.dept_no
+        AND b.dept_no = m.dept_no
+        AND c.to_date LIKE '9999%';
 
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
+-- SELECT 
+--     b.dept_no, AVG(a.salary) AS avg_salary
+-- FROM
+--     salaries a,
+--     dept_emp b
+-- WHERE
+--     a.emp_no = b.emp_no
+--         AND a.to_date LIKE '9999%'
+--         AND b.to_date LIKE '9999%'
+-- GROUP BY b.dept_no
+-- ORDER BY AVG(a.salary) DESC
+-- LIMIT 1
+-- ;
+SELECT 
+    a.emp_no,
+    CONCAT(a.first_name, ' ', a.last_name) AS emp_fullname,
+    b.title,
+    c.salary
+FROM
+    employees a,
+    titles b,
+    salaries c,
+    dept_emp d,
+    (SELECT 
+        b.dept_no, AVG(a.salary) AS avg_salary
+    FROM
+        salaries a, dept_emp b
+    WHERE
+        a.emp_no = b.emp_no
+            AND a.to_date LIKE '9999%'
+            AND b.to_date LIKE '9999%'
+    GROUP BY b.dept_no
+    ORDER BY AVG(a.salary) DESC
+    LIMIT 1) e
+WHERE
+    a.emp_no = b.emp_no
+        AND b.emp_no = c.emp_no
+        AND c.emp_no = d.emp_no
+        AND d.dept_no = e.dept_no
+        AND b.to_date LIKE '9999%'
+        AND c.to_date LIKE '9999%'
+        AND d.to_date LIKE '9999%'
+ORDER BY c.salary DESC
+;
 
 -- 문제6.
 -- 평균 연봉이 가장 높은 부서는? 
-
+-- SELECT 
+--     a.dept_no, AVG(b.salary) AS avg_salary
+-- FROM
+--     dept_emp a,
+--     salaries b
+-- WHERE
+--     a.emp_no = b.emp_no
+-- GROUP BY a.dept_no;
+SELECT 
+    dept_name
+FROM
+    departments a,
+    (SELECT 
+        a.dept_no, AVG(b.salary) AS avg_salary
+    FROM
+        dept_emp a, salaries b
+    WHERE
+        a.emp_no = b.emp_no
+    GROUP BY a.dept_no) b
+WHERE
+    a.dept_no = b.dept_no
+ORDER BY b.avg_salary DESC
+LIMIT 1;
+    
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
+-- SELECT 
+--     b.title, AVG(salary) AS avg_salary
+-- FROM
+--     salaries a,
+--     titles b
+-- WHERE
+--     a.emp_no = b.emp_no
+-- GROUP BY b.title;
+SELECT 
+    a.title
+FROM
+    (SELECT 
+        b.title, AVG(salary) AS avg_salary
+    FROM
+        salaries a, titles b
+    WHERE
+        a.emp_no = b.emp_no
+    GROUP BY b.title) a
+ORDER BY a.avg_salary DESC
+LIMIT 1;
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
+-- SELECT 
+--     a.dept_no,
+--     CONCAT(c.first_name, ' ', c.last_name) AS manager_fullname,
+--     b.salary AS manager_salary
+-- FROM
+--     dept_manager a,
+--     salaries b,
+--     employees c
+-- WHERE
+--     a.emp_no = b.emp_no
+--         AND b.emp_no = c.emp_no
+--         AND a.to_date LIKE '9999%'
+--         AND b.to_date LIKE '9999%';
+SELECT 
+    c.dept_name,
+    CONCAT(a.first_name, ' ', a.last_name) AS emp_fullname,
+    d.salary,
+    e.manager_fullname,
+    e.manager_salary
+FROM
+    employees a,
+    dept_emp b,
+    departments c,
+    salaries d,
+    (SELECT 
+        a.dept_no,
+            CONCAT(c.first_name, ' ', c.last_name) AS manager_fullname,
+            b.salary AS manager_salary
+    FROM
+        dept_manager a, salaries b, employees c
+    WHERE
+        a.emp_no = b.emp_no
+            AND b.emp_no = c.emp_no
+            AND a.to_date LIKE '9999%'
+            AND b.to_date LIKE '9999%') e
+WHERE
+    a.emp_no = b.emp_no
+        AND b.dept_no = c.dept_no
+        AND a.emp_no = d.emp_no
+        AND c.dept_no = e.dept_no
+        AND b.to_date LIKE '9999%'
+        AND d.to_date LIKE '9999%'
+        AND d.salary > e.manager_salary;
